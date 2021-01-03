6d1b3c953ccd7c757fb4b08b6502dbc67b1576fa
Jandolinski <jan.dolinski89@gmail.com>
2021-01-03 12:49:25 +0100

    Extract some methods

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -19,51 +19,103 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (!$this->isAgedBrie($item) and !$this->isBackstagePass($item)) {
                 if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+                    if (!$this->isSulfuras($item)) {
+                        $this->decreaseQuality($item);
                     }
                 }
             } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+                if ($this->qualityLessThan50($item)) {
+                    $this->increaseQuality($item);
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                            if ($this->qualityLessThan50($item)) {
+                                $this->increaseQuality($item);
                             }
                         }
                         if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                            if ($this->qualityLessThan50($item)) {
+                                $this->increaseQuality($item);
                             }
                         }
                     }
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if (!$this->isSulfuras($item)) {
                 $item->sell_in = $item->sell_in - 1;
             }
 
             if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if (!$this->isAgedBrie($item)) {
+                    if (!$this->isBackstagePass($item)) {
                         if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
+                            if (!$this->isSulfuras($item)) {
+                                $this->decreaseQuality($item);
                             }
                         }
                     } else {
                         $item->quality = $item->quality - $item->quality;
                     }
                 } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                    if ($this->qualityLessThan50($item)) {
+                        $this->increaseQuality($item);
                     }
                 }
             }
         }
+    }
+
+    /**
+     * @param Item $item
+     */
+    private function decreaseQuality(Item $item): void
+    {
+        $item->quality = $item->quality - 1;
+    }
+
+    /**
+     * @param Item $item
+     */
+    private function increaseQuality(Item $item): void
+    {
+        $item->quality = $item->quality + 1;
+    }
+
+    /**
+     * @param Item $item
+     * @return bool
+     */
+    private function qualityLessThan50(Item $item): bool
+    {
+        return $item->quality < 50;
+    }
+
+    /**
+     * @param Item $item
+     * @return bool
+     */
+    private function isAgedBrie(Item $item): bool
+    {
+        return $item->name == 'Aged Brie';
+    }
+
+    /**
+     * @param Item $item
+     * @return bool
+     */
+    private function isBackstagePass(Item $item): bool
+    {
+        return $item->name == 'Backstage passes to a TAFKAL80ETC concert';
+    }
+
+    /**
+     * @param Item $item
+     * @return bool
+     */
+    private function isSulfuras(Item $item): bool
+    {
+        return $item->name == 'Sulfuras, Hand of Ragnaros';
     }
 }
